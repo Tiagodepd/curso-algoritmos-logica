1a5590080db2f8da1fb2a7f75fc7013c504e3a1e
Tiago Damasceno <tiagodepd@yahoo.com.br>
2022-03-05 20:53:17 -0300

    resolução exercícios python add

---
+++ Exercícios/6. Python/prbl_somavetor.py
@@ -0,0 +1,19 @@
+N = int(input("Quantos números você vai digitar? "))
+vet = [0 for x in range(N)]
+
+for i in range(0, N):
+  vet[i] = float(input("Digite um número: "))
+
+print()
+print("VETORES = ", end=" ")
+for i in range(0, N):
+  print(f"{vet[i]:.1f} ", end=" ")
+
+print()
+soma = 0
+for i in range(0, N):
+  soma = soma + vet[i]
+print(f"SOMA = {soma:.2f}")
+
+media = soma / N
+print(f"MÉDIA = {media:.2f}")  
